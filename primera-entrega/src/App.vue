3577d5b0ff87ef68dc937914dc63d8b03735fd2d
Paz Spera <mspera03@gmail.com>
2022-05-13 17:26:23 -0300

    agregué template a ProductCard.vue. no logro que recupere la data de los products dentro de las hamburgers. probar tener un objeto en el data de products, que dentro haya otros objetos con los tipos de productos dentro

--- primera-entrega/src/App.vue
+++ primera-entrega/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <ProductCard/>
+    <ProductCard  v-for="(product, p) in products" :key="p"  />
   </div>
 </template>
 
@@ -11,7 +11,83 @@ import ProductCard from "./components/ProductCard.vue";
 export default {
   name: "App",
   components: {
-    ProductCard
+    ProductCard,
+  },
+  data() {
+    return {
+      hamburgers: {
+        products: [
+          {
+            name: "Doble Bacon",
+            description: "2 medallones de carne de 100gr, cheddar, panceta ahumada, cebolla caramelizada",
+            price: "1120",
+          },
+          {
+            name: "Baby Blue",
+            description: "Medallón de carne de 100gr, queso azul, morrón, cebolla caramelizada, pepinillos",
+            price: "1050",
+          },
+          {
+            name: "Locura Not Burger",
+            description: "Medallón not burger, guacamole, tomate, cebolla caramelizada, huevo frito",
+            price: "1050",
+          },
+          {
+            name: "Remolacha Pasión",
+            description: "Medallón de remolacha, tomates cherry, mozzarella",
+            price: "950",
+          },
+        ],
+      },
+      tapas: {
+        products: [
+          {
+            name: "Papas con Cheddar",
+            description: "Papas fritas con salsa cheddar",
+            price: "550",
+          },
+          {
+            name: "Papas Fogosas",
+            description: "Papas fritas con salsa picante",
+            price: "550",
+          },
+          {
+            name: "Nachos Completos",
+            description: "Nachos con queso cheddar y dip de guacamole",
+            price: "750",
+          },
+          {
+            name: "Mozzarellas Pasión",
+            description: "Triángulos de queso mozzarella rebozados (6) con salsa de toma y guacamole",
+            price: "950",
+          },
+        ],
+      },
+      drinks: {
+        products: [
+          {
+            name: "Coca Cola Lata 354ml",
+            price: "250",
+          },
+          {
+            name: "Sprite Lata 354ml",
+            price: "250",
+          },
+          {
+            name: "Agua Botella 500ml",
+            price: "250",
+          },
+          {
+            name: "Porrón Patagonia 500ml",
+            price: "450",
+          },
+          {
+            name: "Botella Patagonia 1 litro",
+            price: "950",
+          },
+        ],
+      },
+    };
   },
 };
 </script>
--- primera-entrega/src/components/ProductCard.vue
+++ primera-entrega/src/components/ProductCard.vue
@@ -2,15 +2,24 @@
   <div class="card" style="width: 18rem">
     <!-- <img src="..." class="card-img-top" alt="..." /> -->
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title">{{ product.name }}</h5>
+      <p class="card-text">{{ product.description }}</p>
+      <p class="card-text">{{ product.price }}</p>
+      <a href="#" class="btn btn-primary">Agregar a carrito</a>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ProductCard",
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style></style>
