650947b00108ff129daaf602fdd6e473d9dc936a
Paz Spera <mspera03@gmail.com>
2022-05-15 17:35:57 -0300

    logré que las imgs de productos aparecieran agregando como parámetro a getImgUrl el imgSrc de cada producto

--- primera-entrega/src/components/ProductCard.vue
+++ primera-entrega/src/components/ProductCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
     <div class="col" v-for="(product, p) in products" :key="p">
-      <div class="card h-100" >
-        <!-- <img :src="getImgUrl()" class="card-img-top" :alt="product.name" /> -->
+      <div class="card h-100">
+        <img :src="getImgUrl(product.imgSrc)" class="card-img-top" :alt="product.name" />
         <div class="card-body">
           <h5 class="card-title">{{ product.name }}</h5>
           <p class="card-text">{{ product.description }}</p>
@@ -24,10 +24,12 @@ export default {
     },
   },
   methods: {
-    getImgUrl() {
-      return require(`@/assets/img/${this.imgSrc}`);
-    }
-  }
+    // Las imgs no estaban funcionando hasta que agregué que en el template getImgUrl reciba como
+    // parámetro el imgSrc de cada producto y lo agregué al final de la ruta
+    getImgUrl(imgSrc) {
+      return require("@/assets/img/" + imgSrc);
+    },
+  },
 };
 </script>
 
