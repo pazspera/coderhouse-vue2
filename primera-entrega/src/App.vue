8670f7b5fbf3ab0000f37191126bedbe53573b2e
Paz Spera <mspera03@gmail.com>
2022-05-17 22:42:04 -0300

    corregí lo que no andaba de la desestructuración, con poner los valores entre llaves ya alcanzaba

--- primera-entrega/src/App.vue
+++ primera-entrega/src/App.vue
@@ -173,8 +173,7 @@ export default {
       let filteredArray = array.filter((product) => product.category === category);
       return filteredArray;
     },
-    updateCart({ productId, productCounter}) {
-      let { productId, productCounter} = productToAdd;
+    updateCart({ productId, productCounter }) {
       // Revisa si el addedProduct ya existe en el cart
       let addedProduct = this.cart.find((product) => product.id === productId);
 
