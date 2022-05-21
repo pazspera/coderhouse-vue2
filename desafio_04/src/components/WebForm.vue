dc77110f02dc8c2852a6bda8b4207b4e41dd0d35
Paz Spera <mspera03@gmail.com>
2022-05-21 18:54:38 -0300

    agregué componente WebForm con estilos bootstrap para inputs nombre, apellido y email

---
+++ desafio_04/src/components/WebForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="container">
+      <form>
+        <!-- Nombre y apellido -->
+        <div class="row mb-3">
+          <div class="col">
+            <label class="form-label" for="name">Nombre</label>
+            <input type="text" class="form-control" name="name" id="name" />
+          </div>
+          <div class="col">
+            <label class="form-label" for="lastName">Apellido</label>
+            <input type="text" class="form-control" name="lastName" id="lastName" />
+          </div>
+        </div>
+        <!-- Email -->
+        <div class="row mb-3">
+          <div class="col">
+            <label class="form-label" for="email">Email</label>
+            <input class="form-control" type="email" name="email" id="email" />
+          </div>
+        </div>
+        <!-- Cursos -->
+        <div class="row mb-3">
+            <div class="col-12">
+                <p>Elige tu curso</p>
+            </div>
+            <div class="col">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="checkJavascript" id="checkJavascript">
+                    <label class="form-check-label" for="checkJavascript">JavaScript</label>
+                </div>
+            </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
