b19a1b1534f4d68ac63033110b458ce3fc51b8bb
Paz Spera <mspera03@gmail.com>
2022-05-21 19:45:41 -0300

    agregué campos faltantes al formulario

--- desafio_04/src/components/WebForm.vue
+++ desafio_04/src/components/WebForm.vue
@@ -20,16 +20,44 @@
             <input class="form-control" type="email" name="email" id="email" />
           </div>
         </div>
+        <!-- País de residencia -->
+        <div class="row mb-3">
+          <div class="col">
+            <p class="form-label">País de residencia</p>
+            <select class="form-select" name="country" id="country">
+              <option selected></option>
+              <option v-for="country in countriesList" :key="country.id" :value="country.name">{{ country.name }}</option>
+            </select>
+          </div>
+        </div>
         <!-- Cursos -->
         <div class="row mb-3">
           <div class="col">
-            <p>Cursos disponibles</p>
+            <p class="form-label">Cursos disponibles</p>
             <div class="form-check" v-for="(course, c) in availableCourses" :key="c">
               <input class="form-check-input" type="checkbox" :name="course.id" :id="course.id" />
               <label class="form-check-label" :for="course.id">{{ course.name }}</label>
             </div>
           </div>
         </div>
+        <!-- Comentarios -->
+        <div class="row mb-3">
+          <div class="col">
+            <label for="comments" class="form-label">Comentarios</label>
+            <textarea class="form-control" id="comments" rows="3"></textarea>
+          </div>
+        </div>
+        <!-- Newsletter -->
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="newsletter" id="newsletter">
+                    <label class="form-check-label" for="newsletter">Suscribime al newsletter</label>
+                </div>
+            </div>
+        </div>
+        <!-- Submit -->
+        <button type="submit" class="btn btn-primary">Enviar</button>
       </form>
     </div>
   </div>
@@ -62,9 +90,35 @@ export default {
           name: "NodeJS",
         },
       ],
+      countriesList: [
+        {
+          id: "a",
+          name: "Argentina",
+        },
+        {
+          id: "b",
+          name: "Uruguay",
+        },
+        {
+          id: "c",
+          name: "Chile",
+        },
+        {
+          id: "d",
+          name: "Perú",
+        },
+        {
+          id: "e",
+          name: "Mexico",
+        },
+      ],
     };
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  max-width: 700px;
+}
+</style>
